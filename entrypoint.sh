4092acb33a5321e6a2c593c2155e36e54e851333
Ivan Grginov <ivan.grginov@infinum.hr>
2020-02-13 23:06:12 +0100

    Learning bash 11

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env sh
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 LIGHTBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 function error() {
-  echo -e "${RED}===== ERROR: ${NC}"
-  echo -e "$1"
-  echo -e "${RED}=====${NC}"
+  echo -e "${RED}===== ERROR: ${NC} $1"
   exit 1
 }
 
 function preflightChecklist() {
 
-  printenv
   echo "---------------------------------------"
   echo "Starting preflightChecklist()"
   echo "---------------------------------------"
-
 
   # -------------------------------------------------------------
   # 1. Check if all environment variables are set
